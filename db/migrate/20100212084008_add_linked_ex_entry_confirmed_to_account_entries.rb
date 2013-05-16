c39691303d079d73f1a126165be60a72b5b4247e
ymmtmsys <m2yamamoto@gmail.com>
2013-05-16 16:53:09 +0900

    Converting MySQL to PostgreSQL

--- db/migrate/20100212084008_add_linked_ex_entry_confirmed_to_account_entries.rb
+++ db/migrate/20100212084008_add_linked_ex_entry_confirmed_to_account_entries.rb
@@ -4,7 +4,7 @@ class AddLinkedExEntryConfirmedToAccountEntries < ActiveRecord::Migration
   def self.up
     add_column :account_entries, :linked_ex_entry_confirmed, :boolean, :null => false, :default => false
 
-    execute "update account_entries inner join deals on account_entries.linked_ex_deal_id = deals.id set linked_ex_entry_confirmed = deals.confirmed"
+    execute "update account_entries set linked_ex_entry_confirmed = deals.confirmed from deals where account_entries.linked_ex_deal_id = deals.id"
   end
 
   def self.down
