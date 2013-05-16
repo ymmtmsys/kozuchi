37f8c71ce8401db5155f2997581abca8c62b5e7f
ymmtmsys <m2yamamoto@gmail.com>
2013-05-16 16:45:13 +0900

    Converting MySQL to PostgreSQL

--- db/migrate/20091123034257_add_type_to_account_entries.rb
+++ db/migrate/20091123034257_add_type_to_account_entries.rb
@@ -3,7 +3,7 @@
 class AddTypeToAccountEntries < ActiveRecord::Migration
   def self.up
     add_column :account_entries, :type, :string
-    execute "update account_entries inner join deals on deals.id = account_entries.deal_id set account_entries.type = deals.type;"
+    execute "update account_entries set account_entries.type = deals.type from deals where deals.id = account_entries.deal_id;"
   end
 
   def self.down
