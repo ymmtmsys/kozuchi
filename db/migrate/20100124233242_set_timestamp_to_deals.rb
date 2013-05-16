b3d8fd26975d267f3b5393e6220c15f8bbaedb4c
ymmtmsys <m2yamamoto@gmail.com>
2013-05-16 16:52:04 +0900

    Converting MySQL to PostgreSQL

--- db/migrate/20100124233242_set_timestamp_to_deals.rb
+++ db/migrate/20100124233242_set_timestamp_to_deals.rb
@@ -3,7 +3,7 @@
 # Ensure deals have non null timestamps.
 class SetTimestampToDeals < ActiveRecord::Migration
   def self.up
-    execute "update deals set created_at = timestamp(date) where created_at is null"
+    execute "update deals set created_at = date::timestamp where created_at is null"
     execute "update deals set updated_at = created_at where updated_at is null"
   end
 
